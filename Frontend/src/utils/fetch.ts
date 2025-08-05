66948bf26711250a4b09035886731dd55b8fee67
marcochippy <hello@marcoczirpek.com>
2025-08-05 17:17:19 +0200

    frontend setup

--- Frontend/src/utils/fetch.ts
+++ Frontend/src/utils/fetch.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getHourly = async () => {
   try {
-    const response = await axios.get('http://api.marcoczirpek.com/hourly');
+    const response = await axios.get('https://api.marcoczirpek.com/hourly');
     console.log(response);
   } catch (error) {
     console.error(error);
