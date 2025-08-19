93eee8b3e5e300f3115ba453fb4aff44a28cd40d
marcochippy <hello@marcoczirpek.com>
2025-08-19 12:18:18 +0200

    Deployment Worker Test

---
+++ Frontend/worker.js
@@ -0,0 +1,15 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+
+addEventListener('fetch', event => {
+  event.respondWith(handleEvent(event));
+});
+
+async function handleEvent(event) {
+  try {
+    return await getAssetFromKV(event);
+  } catch (e) {
+    return await getAssetFromKV(event, {
+      mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/index.html`, req)
+    });
+  }
+}
